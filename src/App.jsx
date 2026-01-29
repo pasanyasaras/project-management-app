378eeb51dd47323af6d4f82078430941dacb5829
Pasan Yasara <pasansmart@gmail.com>
2026-01-29 21:55:45 +0530

    handling project creation and updating the ui

--- src/App.jsx
+++ src/App.jsx
@@ -21,18 +21,20 @@ function App() {
 
   function handleAddProject(projectData){
     setProjectsState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
         id: Math.random()
       }
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject]
       }
     })
   } 
 
-  console.log(projectsState);
+
 
   let content;
 
@@ -43,7 +45,7 @@ function App() {
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
       {content}
     </main>
   );
